0616c755d14cb6e776afeeede3192c2b0e0761c5
Sai <rohithv@auroraelabs.com>
2024-02-23 14:22:34 +0530

    added requirement.txt file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,11 +6,18 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "my_lambda_function" {
-  filename      = "./aws_lambda.zip"  
+  filename      = "${path.module}/python/aws_lambda.zip"  
   function_name = "aws_lambda"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "aws_handler.lambda_handler"      
+  handler       = "aws_lambda.lambda_handler"      
   runtime       = "python3.9"         
+}
+
+
+data "archive_file" "zip_the_python_code" {
+ type        = "zip"
+ source_dir  = "${path.module}/python/"
+ output_path = "${path.module}/python/aws_lambda.zip"
 }
 
 resource "aws_iam_role" "lambda_role" {
